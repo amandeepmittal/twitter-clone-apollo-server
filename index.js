d9eb223dc382ea0e5aa98fc7b36608d2a5464822
Aman Mittal <amandeepmittal@live.com>
2019-05-19 17:40:53 +0530

    hello world example

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const { ApolloServer, gql } = require("apollo-server-express")
 const app = express()
 const port = process.env.PORT || 5000
 
-// Construct a schema, using GraphQL schema language
 const typeDefs = gql`
 	type Query {
 		hello: String
 	}
 `
 
-// Provide resolver functions for your schema fields
 const resolvers = {
 	Query: {
 		hello: () => "Hello world!"
