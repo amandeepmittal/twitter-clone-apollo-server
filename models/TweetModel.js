75ae4886837761dd3e95c7f76eb061e02c10e266
Aman Mittal <amandeepmittal@live.com>
2019-05-19 18:06:48 +0530

    create tweet model

---
+++ models/TweetModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose")
+
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost:27017/twitter", {
+	useNewUrlParser: true
+})
+
+const Schema = mongoose.Schema
+
+const tweetSchema = new Schema({
+	tweet: String,
+	author: String,
+	createdAt: Date
+})
+
+const TweetModel = mongoose.model("Tweet", tweetSchema)
+
+export default {
+	getTweets: () => TweetModel.find().sort({ _id: -1 }),
+	getTweet: _id => TweetModel.findOne({ _id }),
+	createTweet: args => TweetModel(args).save(),
+	deleteTweet: args => {
+		const { _id } = args
+
+		TweetModel.remove({ _id }, error => {
+			if (error) {
+				console.log("Error Removing: ", error)
+			}
+		})
+
+		return args
+	},
+	updateTweet: args => {
+		const { _id, tweet } = args
+
+		TweetModel.update(
+			{ _id },
+			{
+				$set: { tweet }
+			},
+			{ upsert: true },
+			error => {
+				if (error) {
+					console.log("Error Updating: ", error)
+				}
+			}
+		)
+
+		args.author = "user123" // temporary user
+		args.createdAt = new Date()
+
+		return args
+	}
+}
